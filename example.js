3273887fe012788dd3962d2073691bab2ce7eaab
Felix Rieseberg <felix@felixrieseberg.com>
2015-06-25 10:09:01 -0700

    Initial Commit

---
+++ example.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var React = require('react');
+var AzureCreateAppTutorial = require('./src/azuretutorial');
+
+// Render
+React.render(<AzureCreateAppTutorial />, document.getElementById('example'));
